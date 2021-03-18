9b0ce8b6a19b8b6187af3931d6cea11d1f3b0294
Bohdan Opyr <bohdanopyr@gmail.com>
2021-03-18 09:10:55 +0200

    Fixed graphs, the second slide, font-size, added an OpenSans link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { VictoryChart, VictoryLine } from 'victory';
+import { VictoryChart, VictoryLine, VictoryPie, VoctoryTooltip, VictoryLabel, VictoryTooltip, VictoryVoronoiContainer, VictoryGroup, VictoryScatter } from 'victory';
 import getData from './data';
 
 import {
@@ -63,11 +63,12 @@ const template = () => (
   </FlexBox>
 );
 
+const anote = { color: theme.colors.pale, position: "absolute", bottom: "3rem", right: "8rem" };
 const Presentation = () => (
   <Deck theme={theme} template={template}>
     <Slide backgroundColor="background">
       <Heading>FoggyOS</Heading>
-      <FlexBox direction="row" justifyContent="center">
+      <FlexBox flexDirection="row" justifyContent="center">
         <Text>ОС Налаштована бути налаштовуваною</Text>
       </FlexBox>
       <Notes>
@@ -81,19 +82,19 @@ const Presentation = () => (
     <Slide backgroundColor="background">
       <Heading>Будуємо на: GNU/Linux</Heading>
       <UnorderedList>
-        <ListItem>
+        <ListItem style={{ fontSize: 28 }}>
           Linux - операційні системи на основі однойменного ядра. Це - один із найвидатніших прикладів розробки вільного та відкритого програмного забезпечення
-        </ListItem>
-        <ListItem>
+          </ListItem>
+        <ListItem style={{ fontSize: 28 }}>
           GNU - операційна система, що є <i>вільним</i> програмним забезпеченням, тобто поважає свободу користувача
-        </ListItem>
-        <ListItem>
+          </ListItem>
+        <ListItem style={{ fontSize: 28 }}>
           Дистрибутив Linux (часто скорочується як distro) - це операційна система, створена з колекції програмного забезпечення, яка базується на ядрі Linux і, часто, системі управління пакетами
         </ListItem>
-        <small>Визначення: вікіпедія, вебсайт gnu.org</small>
       </UnorderedList>
+      <small><a style={anote}>Визначення: вікіпедія, вебсайт gnu.org</a></small>
       <Notes>
-        <a style={{ color: theme.colors.main }} href="https://www.howtogeek.com/132624/htg-explains-whats-a-linux-distro-and-how-are-they-different/">What is a linux distro</a>
+        <a href="https://www.howtogeek.com/132624/htg-explains-whats-a-linux-distro-and-how-are-they-different/">What is a linux distro</a>
         <ul>
           <li>Складно будувати усе від початку</li>
           <li>Ми вирішили будувати на основі GNU/linux - наборі вільного програмного забезпечення</li>
@@ -105,10 +106,10 @@ const Presentation = () => (
       </Notes>
     </Slide>
     <Slide backgroundColor="background">
-      <FlexBox direction="column" alignItems="center">
-        <Text>Ubuntu 20.10, "Groovy Gorilla", GNOME desktop</Text>
+      <FlexBox flexDirection="column" alignItems="center">
+        <Text>Ubuntu, GNOME desktop</Text>
       </FlexBox>
-      <FlexBox direction="column" alignItems="center">
+      <FlexBox flexDirection="row" alignItems="center">
         <img src="https://upload.wikimedia.org/wikipedia/commons/9/97/Ubuntu_20.10_2880p_EN_31_12_2020_13_51_50.png" width="50%" />
         <Quote><small>Ubuntu - операційна система, найпопулярніший у світі дистрибутив Linux. Серед основних цілей Ubuntu - надання сучасного й водночас стабільного програмного забезпечення для пересічного користувача із сильним акцентом на простоту встановлення та користування</small></Quote>
       </FlexBox>
@@ -121,13 +122,35 @@ const Presentation = () => (
       </Notes>
     </Slide>
     <Slide backgroundColor="background">
-      <Heading>Падіння Windows</Heading>
-      <VictoryChart width={600} height={250}>
-        <VictoryLine data={getData()} x="date" y="windows"></VictoryLine>
-        <VictoryLine data={getData()} x="date" y="osx"></VictoryLine>
-        <VictoryLine data={getData()} x="date" y="linux"></VictoryLine>
-      </VictoryChart>
-      <small><a href="https://gs.statcounter.com/os-market-share/desktop/worldwide/#quarterly-200901-202004" style={{ color: theme.colors.pale, position: "absolute", bottom: "3rem", right: "8rem" }}>Джерело: GlobalStats Statcounter</a></small>
+      <FlexBox flexDirection="column" alignItems="center">
+        <Heading>Падіння Windows</Heading>
+        <FlexBox flexDirection="row" alignItems="center">
+          <Box style={{ width: "800px", height: "500px" }}>
+            <VictoryChart width={800} height={500} containerComponent={<VictoryVoronoiContainer />}>
+              <VictoryGroup color="#3a31c4" data={getData().map((e) => ({ x: e.date, y: e.windows }))} labels={({ datum }) => `windows: ${datum.y}`} labelComponent={<VictoryTooltip style={{ fontSize: 10 }} />}><VictoryLine /><VictoryScatter size={({ active }) => active ? 8 : 3} /></VictoryGroup>
+              <VictoryGroup color="#3ac431" data={getData().map((e) => ({ x: e.date, y: e.other }))} labels={({ datum }) => `інші: ${datum.y}`} labelComponent={<VictoryTooltip style={{ fontSize: 10 }} />}><VictoryLine /><VictoryScatter size={({ active }) => active ? 8 : 3} /></VictoryGroup>
+            </VictoryChart>
+          </Box>
+          <Box style={{ width: "500px", height: "400px" }}>
+            <FlexBox flexDirection="column" height="100%">
+              <Text>Розподіл інших ос</Text>
+              <VictoryPie
+                style={{ labels: { fill: "white" } }}
+                innerRadius={100}
+                labelRadius={120}
+                labels={({ datum }) => `${datum.name}`}
+                labelComponent={<VictoryLabel />}
+                data={[
+                  { x: 1, y: 1.98, name: "chrome os" },
+                  { x: 2, y: 16.74, name: "osx" },
+                  { x: 3, y: 1.99, name: "linux" },
+                ]}
+              />
+            </FlexBox>
+          </Box>
+        </FlexBox>
+      </FlexBox>
+      <small><a href="https://gs.statcounter.com/os-market-share/desktop/worldwide/#quarterly-200901-202004" style={anote}>Джерело: GlobalStats Statcounter</a></small>
       <Notes>
         <ul>
           <li>В 2010 віндовс покривав 93% ринку, а зараз - 76%. Головним конкурентом є macOs - продукт компанії apple, що за цей період зріс з 5 до 18%. Linux ж зріс від ⅔ до 3.5% - більш, ніж у п'ять разів. В Україні ж віндовс опустився з 98% до 85%, а частка лінукс за деякими даними сягає 4.5%. Більшості вибір простий - або віндовс, або мак. Мак - система, яка не по кишені багатьом. Тому ми будуємо систему, яка, будучи такою ж зручною, є цілком безкоштовною</li>
